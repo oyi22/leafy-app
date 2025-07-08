0f359f573bdcc97260c7579e264d930fbfce3ce7
oyi22 <oyinanjam.com>
2025-07-08 07:40:56 +0700

    update app

--- app.py
+++ app.py
@@ -90,4 +90,6 @@ def delete_item(id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(debug=False, host="0.0.0.0", port=port)
